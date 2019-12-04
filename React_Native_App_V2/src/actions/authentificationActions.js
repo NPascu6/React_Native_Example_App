c2673aaff061366ed18c562f3abd5fd21200e107
NHome <norbipascu92@gmail.com>
2019-12-04 21:58:12 +0200

    Added user List as json

--- React_Native_App_V2/src/actions/authentificationActions.js
+++ React_Native_App_V2/src/actions/authentificationActions.js
@@ -3,27 +3,28 @@ export const LOGIN_SUCCESS_USER = 'LOGIN_SUCCESS_USER';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const LOGOUT = 'LOGOUT';
 
-import {users as userList} from '../components/Login/login_components/userList.json';
+import { users as userList } from '../components/Login/login_components/userList.json';
 
 export function login(userCredentials) {
 
     debugger;
-    let x = userList;
-    if (userCredentials.userName === 'test' && userCredentials.password === 'test') {
-        return {
-            type: LOGIN_SUCCESS_ADMIN
+
+    for (let user of userList) {
+        if (userCredentials.userName === user.UserName && userCredentials.password === user.Password && user.Role === "Admin") {
+            return {
+                type: LOGIN_SUCCESS_ADMIN
+            }
         }
-    }
-    else if (userCredentials.userName === 'test' && userCredentials.password === 'test1234') {
-        return {
-            type: LOGIN_SUCCESS_USER
+        else if (userCredentials.userName === user.UserName && userCredentials.password === user.Password && user.Role === "User") {
+            return {
+                type: LOGIN_SUCCESS_USER
+            }
         }
+    };
+    return {
+        type: LOGIN_ERROR
     }
-    else {
-        return {
-            type: LOGIN_ERROR
-        }
-    }
+
 }
 
 export function logout() {
