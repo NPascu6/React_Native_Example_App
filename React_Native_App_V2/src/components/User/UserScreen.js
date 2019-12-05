53231f536043b5b7dd2db3e14eab0076f5de8985
Pascu Norbert <norbert.pascu@softvision.ro>
2019-12-05 11:19:54 +0200

    Added actions for user with Redux

--- React_Native_App_V2/src/components/User/UserScreen.js
+++ React_Native_App_V2/src/components/User/UserScreen.js
@@ -8,23 +8,38 @@ import * as ActionCreators from '../../actions/userActions';
 //Components
 import LogoutButton from '../Shared_Components/LogoutButton';
 
-mapStateToProps = (state) => { return { action: state.userReducers.action } }
+mapStateToProps = (state) => { return { userState: state.userReducers.userState } }
 mapDispatchToProps = (dispatch) => { return bindActionCreators(ActionCreators, dispatch); }
 
 class UserScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            action: ""
+            userState: {
+                action:""
+            }
         }
+    }
+
+    componentDidMount() {
+        debugger;
+        this.setState({ userState: this.props.userState });
     }
 
     getAction1 = () => {
         debugger;
         this.props.userAction1();
-
-        debugger;                                                                               
-        this.setState({action: this.props.action});
+        this.setState({ userState: this.props.userState });
+    }
+    getAction2 = () => {
+        debugger;
+        this.props.userAction2();
+        this.setState({ userState: this.props.userState });
+    }
+    getAction3 = () => {
+        debugger;
+        this.props.userAction3();
+        this.setState({ userState: this.props.userState });
     }
 
     render() {
@@ -32,7 +47,9 @@ class UserScreen extends Component {
             <View>
                 <LogoutButton navigation={this.props.navigation} />
                 <Text>User Screen</Text>
-                <Text onPress={this.getAction1}>Press me  title={this.state.action}</Text>
+                <Text onPress={this.getAction1}>Action 1:{this.state.userState.action}</Text>
+                <Text onPress={this.getAction2}>Action 2:{this.state.userState.action}</Text>
+                <Text onPress={this.getAction3}>Action 3:{this.state.userState.action}</Text>
             </View>
         )
     }
--- React_Native_App_V2/src/reducers/userReducers.js
+++ React_Native_App_V2/src/reducers/userReducers.js
@@ -1,16 +1,32 @@
-import { USER_ACTION_1 } from '../actions/userActions';
+import { 
+    USER_ACTION_1,
+    USER_ACTION_2,
+    USER_ACTION_3
+} 
+from '../actions/userActions';
 
-let newState = { 
-    action: "initialStateFrom R"
+let newState = {
+    userState: {
+        action: "init"
+    }
 };
 
 export default (state, action) => {
     debugger;
     switch (action.type) {
         case USER_ACTION_1:
-            newState.action = "ActionFrom Redux State";
+            newState.userState.action = "Action 1 from redux";
+            return newState;
+
+        case USER_ACTION_2:
+            newState.userState.action = "Action 2 from redux";
+            return newState;
+
+        case USER_ACTION_3:
+            newState.userState.action = "Action 3 from redux";
             return newState;
         default:
             return state || newState;
     }
 };
+
